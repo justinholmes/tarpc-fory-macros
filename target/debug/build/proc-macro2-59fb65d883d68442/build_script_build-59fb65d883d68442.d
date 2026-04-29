ca4b74a418bf2eac6c7beae15858f3c55447e36b
Justin Holmes <justin@nascency.co.uk>
2026-04-29 13:06:31 +0200

    chore: add .gitignore, remove target/

--- target/debug/build/proc-macro2-59fb65d883d68442/build_script_build-59fb65d883d68442.d
+++
@@ -1,5 +0,0 @@
-/mnt/e/work/fory-rpc/tarpc-fory-macros/target/debug/build/proc-macro2-59fb65d883d68442/build_script_build-59fb65d883d68442.d: /home/justin/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/proc-macro2-1.0.106/build.rs
-
-/mnt/e/work/fory-rpc/tarpc-fory-macros/target/debug/build/proc-macro2-59fb65d883d68442/build_script_build-59fb65d883d68442: /home/justin/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/proc-macro2-1.0.106/build.rs
-
-/home/justin/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/proc-macro2-1.0.106/build.rs:
--- target/debug/libtarpc_fory_macros.d
+++
@@ -1,1 +0,0 @@
-/mnt/e/work/fory-rpc/tarpc-fory-macros/target/debug/libtarpc_fory_macros.so: /mnt/e/work/fory-rpc/tarpc-fory-macros/src/lib.rs
